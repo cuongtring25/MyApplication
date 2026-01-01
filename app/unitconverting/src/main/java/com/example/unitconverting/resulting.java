cd7df4e0fd0d2cde92900114d37465a817e66d76
cuongtring25 <cuongtrinh574@gmail.com>
2026-01-01 12:12:35 +0700

    exercise 1: converter

--- app/unitconverting/src/main/java/com/example/unitconverting/resulting.java
+++ app/unitconverting/src/main/java/com/example/unitconverting/resulting.java
@@ -2,8 +2,7 @@ package com.example.unitconverting;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivityConverter extends AppCompatActivity {
+public class resulting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main_converter);
+        setContentView(R.layout.activity_resulting);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        EditText editTextCelsius = findViewById(R.id.editTextCelsius);
-        Button commitBtn = findViewById(R.id.commitBtn);
-        commitBtn.setOnClickListener(v->{
-            double celsius = Double.parseDouble(editTextCelsius.getText().toString());
-            double faraheit = (celsius * 1.8) + 32;
-            Intent intent = new Intent(this, resulting.class);
-            intent.putExtra("faraheit", faraheit);
-            startActivity(intent);
-        });
+        Intent intent = getIntent();
+
+        double farenheit = intent.getDoubleExtra("faraheit",0);
+
+        TextView tvResult = findViewById(R.id.textView2);
+
+        tvResult.setText(""+ farenheit);
+
     }
 }
